725aadcbd0a80f7c2bfc4ead818088137060e370
Paddy O'Brien <paddy@500px.com>
2012-08-14 14:20:31 -0400

    Initial Commit

---
+++ PXParallaxViewController/PXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PXAppDelegate.h
+//  PXParallaxViewController
+//
+//  Created by Paddy O'Brien on 2012-08-14.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PXParallaxViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PXParallaxViewController
+//
+//  Created by Paddy O'Brien on 2012-08-14.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+	}
+}
